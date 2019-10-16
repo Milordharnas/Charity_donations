eb81dd5847f459236ca82f83d9872b227bc61262
milordharnas <rafalherbut96@gmail.com>
2019-10-16 20:15:32 +0200

    Only models.

---
+++ base_charity_donation/donation/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
+# Create your models here.
+
+INSTITUTION_TYPES = (
+    (1, "fundacja"),
+    (2, "organizacja pozarządowa"),
+    (3, "zbiórka lokalna"),
+)
+
+class Categorie(models.Model):
+    name = models.CharField(max_length=128)
+
+class Institution(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(default = None)
+    types = models.IntegerField(choices=INSTITUTION_TYPES, default=1)
+    categories = models.ManyToManyField(Categorie)
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Categorie)
+    institution = models.ForeignKey(Institution,on_delete=models.CASCADE)
+    address = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=32)
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=32)
+    pick_up_date = models.DateField()
+    pick_up_time = models.DateTimeField()
+    pick_up_comment = models.TextField(null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
